167d7f1e9bfb6374ed69c43c576ac231be91b303
madhuraman87 <madhuraman87@gmail.com>
2014-10-13 22:23:29 -0700

    Remove from cart working correctly

--- src/main/webapp/assets/js/cart_info.js
+++ src/main/webapp/assets/js/cart_info.js
@@ -1,6 +1,14 @@
 $( document ).ready(function() {
 	var user_mailId = sessionStorage.getItem('user_mailId');
 	$.getJSON("http://localhost:8080/CloudOnlineStore/rest/users/displayCart?mailId="+user_mailId, function(data) {
+		//fill dropdown menu for remove from cart
+		$.each(data, function(index, product) {
+			var productId = product.productId;
+			$("#productId_selector")
+			.append($('<option>', { productId : index }).text(productId));
+		});
+
+		//display cart table
 		$("#cartTable").mrjsontable({	
             columns: [
                 new $.fn.mrjsontablecolumn({
@@ -32,3 +40,23 @@ $( document ).ready(function() {
         });
 	});
 });
+
+$("#removeFromCartButton").click(
+		function() {
+			var productIdToBeRemoved = $("#productId_selector").val();
+			var user_mailId = sessionStorage.getItem("user_mailId");
+			var removeFromCartURL = "http://localhost:8080/CloudOnlineStore/rest/users/removeFromCart?mailId=" + user_mailId + "&productId=" + productIdToBeRemoved;
+
+			$.ajax({
+				type: "POST",
+				url: removeFromCartURL,
+				contentType: "application/json",
+				success: function(data, textStatus, jqXHR) {
+					alert("Product removed from cart");
+					location.reload();
+				},
+				error: function(textStatus, jqXHR, errorThrown) {
+					alert(textStatus + " " + jqXHR);
+				}
+				});
+		});
